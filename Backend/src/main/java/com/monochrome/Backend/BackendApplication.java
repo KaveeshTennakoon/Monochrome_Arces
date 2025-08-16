2d82975c3aaa7ea8d97e44eff186edb78239b447
KaveeshTennakoon <kaveeshoffical@gmail.com>
2025-08-16 21:14:25 +0530

    Fix: file naming

--- Backend/src/main/java/com/monochrome/Backend/BackendApplication.java
+++ Backend/src/main/java/com/monochrome/Backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.monochrome.backend;
+package com.monochrome.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
